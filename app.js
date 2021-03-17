cbc698543b0384444926ab61247e5fdc88de2666
Kyle Ohlwiler <kohlwiler@yahoo.com>
2021-03-17 16:15:47 -0700

    updated functions

--- app.js
+++ app.js
@@ -10,18 +10,30 @@ let start = () => {
       type: 'list',
       name: 'action',
       message: 'What would you like to do?',
-      choices: ['Add to', 'View', 'Update Roles', 'Exit']
+    choices: ['Add Department', 'Add Role', 'Add Employee', 'View Departments', 'View Roles', 'View Employees','Update Employee Role', 'Exit']
     })
   .then(({ action }) => {
     switch (action) {
-      case 'Add to':
-        add()
+      case 'Add Department':
+        addDepartment()
         break
-      case 'View':
-        view()
+      case 'Add Role':
+        addRole()
         break
-      case 'Update Roles':
-        updateRoles()
+      case 'Add Employee':
+        addEmployee()
+        break
+      case 'View Departments':
+        viewDepartments()
+        break
+      case 'View Roles':
+        viewRoles()
+        break
+      case 'View Employees':
+        viewEmployees()
+        break
+      case 'Update Employee Role':
+        updateEmployee()
         break
       case 'Exit':
         process.exit()
@@ -31,74 +43,130 @@ let start = () => {
 }
 
 
-let add = () => {
-  prompt({
-    type: 'list',
-    name: 'action',
-    message: 'What would you like to add to?',
-      choices: ['Add to Departments', 'Add to Roles', 'Add to Employees']
-  })
-  .then(({ action }) => {
-    switch (action) {
-      case 'Add to Departments':
-        addDepartment()
-        break
-      case 'Add to Roles':
-        addRole()
-        break
-      case 'Add to Employees':
-        addEmployee()
-        break
-    }
-  })
-  .catch(err => console.log(err))
-}
-
-let view = () => {
-  prompt({
-    type: 'list',
-    name: 'action',
-    message: 'What would you like to view?',
-    choices: ['Departments', 'Roles', 'Employees']
-  })
-    .then(({ action }) => {
-      switch (action) {
-        case 'Departments':
-          viewDepartment()
-          break
-        case 'Add to Roles':
-          viewRole()
-          break
-        case 'Add to Employees':
-          viewEmployee()
-          break
+let updateEmployee = () => {
+  db.query('SELECT * FROM employees', (err, data) => {
+    if (err) { console.log(err) }
+    prompt([
+      {
+        type: 'list',
+        name: 'roleChosen',
+        message: 'Which employee would you like to update?',
+        choices: data.map(employee => ({
+          title: `${employee.title}`,
+          value: employee.id
+        }))
+      },
+      {
+        type: 'input',
+        name:'employee',
+        message: 'What is the new role ID for the employee?'
       }
+    ])
+    .then(({roleChosen}) => {
+      db.query(`UPDATE employees WHERE role_id = ${roleChosen.id}`)
     })
-    .catch(err => console.log(err))
-}
-
-let updateRoles = () => {
+  })
 
 }
 
 let addDepartment = () => {
+  prompt([
+    {
+      type: 'input',
+      name: 'add',
+      message: 'What is the name of the department you want to add?'
+    }
+  ])
+  .then(department => {
+    let newDepartment = {
+      name: department.add
+    }
+    db.query('INSERT INTO departments SET ?', newDepartment, (err) => {
+      if (err) { console.log(err) }
+      console.log('New department has been added')
+      start()
+    })
+  }) 
   
 }
 let addRole = () => {
+  prompt([
+    {
+      type: 'input',
+      name: 'newRole',
+      message: 'What is the title of the role you want to add?'
+    },
+    {
+      type: 'number',
+      name: 'salary',
+      message: 'What is the salary for this role?'
+    },
+    {
+      type: 'number',
+      name: 'id',
+      message: 'What is the department id for this role?'
+    }
+  ])
+    .then(role => {
+      let newRole = {
+        title: role.newRole,
+        salary: role.salary,
+        department_id: role.id
+      }
+      db.query('INSERT INTO roles SET ?', newRole, (err) => {
+        if (err) { console.log(err) }
+        console.log('New role has been added')
+        start()
+      })
+    })
 
 }
 let addEmployee = () => {
-
+  prompt([
+    {
+      type: 'input',
+      name: 'firstName',
+      message: 'What is the first name of the employee?'
+    },
+    {
+      type: 'input',
+      name: 'lastName',
+      message: 'What is the last name of the employee?'
+    },
+    {
+      type: 'number',
+      name: 'roleId',
+      message: 'What is the role ID for this employee?'
+    },
+    {
+      type: 'number',
+      name: 'managerId',
+      message: "What is this employee's manager ID?"
+    }
+  ])
+    .then(employee => {
+      let newEmployee = {
+        first_name: employee.firstName,
+        last_name: employee.lastName,
+        role_id: employee.roleId, 
+        manager_id: employee.managerId
+      }
+      db.query('INSERT INTO employees SET ?', newEmployee, (err) => {
+        if (err) { console.log(err) }
+        console.log('New employee has been added')
+        start()
+      })
+    })
 }
 
-let viewDepartment = () => {
+let viewDepartments = () => {
   db.query('SELECT * FROM departments', (err, data) => {
     if (err) { console.log(err) }
     console.table(data)
     start()
   })
 }
-let viewRole = () => {
+let viewRoles = () => {
   db.query('SELECT * FROM roles', (err, data) => {
     if (err) { console.log(err) }
     console.table(data)
@@ -106,15 +174,15 @@ let viewRole = () => {
   })
 }
 
-let viewEmployee = () => {
+let viewEmployees = () => {
   db.query(`
     SELECT employees.id, employees.first_name, employees.last_name, roles.title, roles.salary, departments.name AS 'department', CONCAT(manager.first_name, ' ', manager.last_name) AS manager
     FROM employees
     LEFT JOIN roles ON employees.role_id = roles.id
     LEFT JOIN departments ON roles.department_id = departments.id
-    LEFT JOIN employees manager ON manager.id = employees.manager_id`, (err, data) => {
+    LEFT JOIN employees manager ON manager.id = employees.manager_id`, (err, employees) => {
       if (err) { console.log(err) }
-      console.table(data)
+      console.table(employees)
       start()
   })
 }
