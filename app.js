06af523a73c526858674ce4b47fc93de25394453
Kyle Ohlwiler <kohlwiler@yahoo.com>
2021-03-16 20:42:03 -0700

    added functions for viewing

--- app.js
+++ app.js
@@ -9,31 +9,19 @@ let start = () => {
   prompt({
       type: 'list',
       name: 'action',
-      message: 'What would you like to do?'
-      choices: ['Add Department', 'Add Role', 'Add Employee', 'View Departments', 'View Roles', 'View Employees', 'Update Employee Roles', 'Exit']
+      message: 'What would you like to do?',
+      choices: ['Add to', 'View', 'Update Roles', 'Exit']
     })
   .then(({ action }) => {
     switch (action) {
-      case 'Add Department':
-        addDepartment()
+      case 'Add to':
+        add()
         break
-      case 'Add Role':
-        addRole()
+      case 'View':
+        view()
         break
-      case 'Add Employee':
-        addEmployee()
-        break
-      case 'View Department':
-        viewDepartment()
-        break
-      case 'View Roles':
-        viewRoles()
-        break
-      case 'view Employees':
-        addDepartment()
-        break
-      case 'Update Employee Roles':
-        addDepartment()
+      case 'Update Roles':
+        updateRoles()
         break
       case 'Exit':
         process.exit()
@@ -42,8 +30,59 @@ let start = () => {
   .catch(err => console.log(err))
 }
 
-let addDepartment = () => {
 
+let add = () => {
+  prompt({
+    type: 'list',
+    name: 'action',
+    message: 'What would you like to add to?',
+      choices: ['Add to Departments', 'Add to Roles', 'Add to Employees']
+  })
+  .then(({ action }) => {
+    switch (action) {
+      case 'Add to Departments':
+        addDepartment()
+        break
+      case 'Add to Roles':
+        addRole()
+        break
+      case 'Add to Employees':
+        addEmployee()
+        break
+    }
+  })
+  .catch(err => console.log(err))
+}
+
+let view = () => {
+  prompt({
+    type: 'list',
+    name: 'action',
+    message: 'What would you like to view?',
+    choices: ['Departments', 'Roles', 'Employees']
+  })
+    .then(({ action }) => {
+      switch (action) {
+        case 'Departments':
+          viewDepartment()
+          break
+        case 'Add to Roles':
+          viewRole()
+          break
+        case 'Add to Employees':
+          viewEmployee()
+          break
+      }
+    })
+    .catch(err => console.log(err))
+}
+
+let updateRoles = () => {
+
+}
+
+let addDepartment = () => {
+  
 }
 let addRole = () => {
 
@@ -52,19 +91,32 @@ let addEmployee = () => {
 
 }
 
-let viewDepartments = () => {
-  
+let viewDepartment = () => {
+  db.query('SELECT * FROM departments', (err, data) => {
+    if (err) { console.log(err) }
+    console.table(data)
+    start()
+  })
 }
-let viewRoles = () => {
-
-}
-let viewEmployees = () => {
-
-}
-
-let updateEmployeeRoles = () => {
-
+let viewRole = () => {
+  db.query('SELECT * FROM roles', (err, data) => {
+    if (err) { console.log(err) }
+    console.table(data)
+    start()
+  })
 }
 
+let viewEmployee = () => {
+  db.query(`
+    SELECT employees.id, employees.first_name, employees.last_name, roles.title, roles.salary, departments.name AS 'department', CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employees
+    LEFT JOIN roles ON employees.role_id = roles.id
+    LEFT JOIN departments ON roles.department_id = departments.id
+    LEFT JOIN employees manager ON manager.id = employees.manager_id`, (err, data) => {
+      if (err) { console.log(err) }
+      console.table(data)
+      start()
+  })
+}
 
 start()
